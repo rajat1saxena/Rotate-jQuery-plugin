11975fc5209587774c4fcedc7645541824ea7870
Rajat Saxena <rajat.saxena.work@gmail.com>
2011-10-07 16:14:45 +0530

    Rotate plugin Version 1.0

---
+++ rotate.jQuery.js
@@ -0,0 +1,35 @@
+(function($){
+	$.fn.rotate = function(angle,axis,duration,type,delay){
+		/*setting elements properties*/
+
+		/*following two lines set rotation properties*/
+		transition = "-webkit-transform "+duration+" "+type+" "+delay;
+		this.css("-webkit-transition",transition);
+		transition = "-moz-transform "+duration+" "+type+" "+delay;
+		this.css("-moz-transition",transition);
+		transition = "-o-transform "+duration+" "+type+" "+delay;
+		this.css("-o-transition",transition);
+		transition = "transform "+duration+" "+type+" "+delay;
+		this.css("transition",transition);
+
+		/*following line change the axis of rotation*/
+		this.css("-webkit-transform-origin",axis);
+		this.css("-moz-transform-origin",axis);
+		this.css("-o-transform-origin",axis);
+		this.css("transform-origin",axis);
+
+		/**/
+
+		/*basic rotation*/
+		var rotangle = "rotate("+angle+"deg)";
+
+		this.css("-webkit-transform",rotangle);
+		this.css("-moz-transform",rotangle);
+		this.css("-o-transform",rotangle);
+		this.css("transform",rotangle);
+
+		/*rotation ends here*/
+
+		return this;
+	}
+})(jQuery);
