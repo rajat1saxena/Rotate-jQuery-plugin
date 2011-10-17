a5f1529ef8161adf7d7f35c643c2db152544ff57
Rajat Saxena <rajat.saxena.work@gmail.com>
2011-10-17 09:00:37 +0530

    Now includes default options

--- rotate.jQuery.js
+++ rotate.jQuery.js
@@ -3,34 +3,47 @@
 /* License: GNU GPL v2 */
 
 (function($){
-	$.fn.rotate = function(angle,axis,duration,type,delay){
+	$.fn.rotate = function(options){
 
-		/*following two lines set rotation properties*/
-		transition = "-webkit-transform "+duration+" "+type+" "+delay;
-		this.css("-webkit-transition",transition);
-		transition = "-moz-transform "+duration+" "+type+" "+delay;
-		this.css("-moz-transition",transition);
-		transition = "-o-transform "+duration+" "+type+" "+delay;
-		this.css("-o-transition",transition);
-		transition = "transform "+duration+" "+type+" "+delay;
-		this.css("transition",transition);
+		var defaults = {
+			angle: 90,
+			axis: 'top left',
+			duration: '500ms',
+			type: 'ease-in',
+			delay: '0ms'
+		}
+		var options = $.extend(defaults,options);
 
-		/*following lines change the axis of rotation*/
-		this.css("-webkit-transform-origin",axis);
-		this.css("-moz-transform-origin",axis);
-		this.css("-o-transform-origin",axis);
-		this.css("transform-origin",axis);
+		return this.each(function(){
+					
+					obj = $(this);
 
-		/*basic rotation*/
-		var rotangle = "rotate("+angle+"deg)";
+					/*following two lines set rotation properties*/
+					transition = "-webkit-transform "+options.duration+" "+options.type+" "+options.delay;
+					obj.css("-webkit-transition",transition);
+					transition = "-moz-transform "+options.duration+" "+options.type+" "+options.delay;
+					obj.css("-moz-transition",transition);
+					transition = "-o-transform "+options.duration+" "+options.type+" "+options.delay;
+					obj.css("-o-transition",transition);
+					transition = "transform "+options.duration+" "+options.type+" "+options.delay;
+					obj.css("transition",transition);
 
-		this.css("-webkit-transform",rotangle);
-		this.css("-moz-transform",rotangle);
-		this.css("-o-transform",rotangle);
-		this.css("transform",rotangle);
+					/*following lines change the axis of rotation*/
+					obj.css("-webkit-transform-origin",options.axis);
+					obj.css("-moz-transform-origin",options.axis);
+					obj.css("-o-transform-origin",options.axis);
+					obj.css("transform-origin",options.axis);
 
-		/*rotation ends here*/
+					/*basic rotation*/
+					var rotangle = "rotate("+options.angle+"deg)";
 
-		return this;
+					obj.css("-webkit-transform",rotangle);
+					obj.css("-moz-transform",rotangle);
+					obj.css("-o-transform",rotangle);
+					obj.css("transform",rotangle);
+
+					/*rotation ends here*/
+		});
+
 	}
 })(jQuery);
